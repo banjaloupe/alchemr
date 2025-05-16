9a6a407156701ee05a7054b268242b1a6a0b4215
Brian <banjaloupe@gmail.com>
2025-05-16 17:14:00 -0500

    more missing pkg refs

--- R/get_alchemer_responses.R
+++ R/get_alchemer_responses.R
@@ -13,8 +13,8 @@ get_alchemer_responses <- function(id, page, resultsperpage) {
   if (Sys.getenv('ALCHEMER_KEY') == "") {
     print("ERROR: Missing Alchemer authentication from alchemer_auth() function")
   } else if (stringr::str_length(Sys.getenv('ALCHEMER_KEY') > 0)) {
-    response <- httr::GET(url = str_glue("https://api.alchemer.com/v5/survey/{id}/surveyresponse?api_token={Sys.getenv('ALCHEMER_KEY')}&api_token_secret={Sys.getenv('ALCHEMER_SECRET')}&page={page}&resultsperpage={resultsperpage}")) |>
-      content("parsed")
+    response <- httr::GET(url = stringr::str_glue("https://api.alchemer.com/v5/survey/{id}/surveyresponse?api_token={Sys.getenv('ALCHEMER_KEY')}&api_token_secret={Sys.getenv('ALCHEMER_SECRET')}&page={page}&resultsperpage={resultsperpage}")) |>
+      httr::content("parsed")
     responses <- as.data.frame(do.call(rbind, response$data))
   } else {
     print("ERROR: Failed to get Alchemer responses")
--- R/total_alchemer_responses.R
+++ R/total_alchemer_responses.R
@@ -11,8 +11,8 @@ total_alchemer_responses <- function(id) {
   if (Sys.getenv('ALCHEMER_KEY') == "") {
     print("ERROR: Missing Alchemer authentication from alchemer_auth() function")
   } else if (stringr::str_length(Sys.getenv('ALCHEMER_KEY') > 0)) {
-    response <- httr::GET(url = str_glue("https://api.alchemer.com/v5/survey/{id}?api_token={Sys.getenv('ALCHEMER_KEY')}&api_token_secret={Sys.getenv('ALCHEMER_SECRET')}")) |>
-      content()
+    response <- httr::GET(url = stringr::str_glue("https://api.alchemer.com/v5/survey/{id}?api_token={Sys.getenv('ALCHEMER_KEY')}&api_token_secret={Sys.getenv('ALCHEMER_SECRET')}")) |>
+      httr::content()
     sum(response$data$statistics$Partial, response$data$statistics$Disqualified, response$data$statistics$Complete)
   } else {
     print("ERROR: Failed to get Alchemer responses")
